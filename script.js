9c40c97cd44ac9406779e22656772673a061aad6
zacharye08 <zjervin@gmail.com>
2016-06-30 22:56:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var b2Vec2 = Box2D.Common.Math.b2Vec2,
 
 var ishipbase, iturret, itractorbeam, istar, ibeam, isplitter, iheavy, igatling, iincinerator, imissile, iufo, iufoeye, iaim, ihealthbar;
 var fxbeam, fxheavy, fxgatling, fxspark, fxfire, fxmissile, fxhealth, fxcoin;
-var gos, counters;
+var gos, counters, buttons;
 var c, ctx;
 var mouse;
 var world;
@@ -23,18 +23,39 @@ var scale = 32;
 var viewscale = 1, viewslot;
 var devtools;
 var spawner;
+var lastT, delta;
+var bounds;
+var mute, paused;
+var boxes;
 var omega = false, omegaunlock = "";
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var version = "0.7.4.5";
+var version = "0.7.4.9";
 
 function init(){
-	document.getElementById("version").innerHTML = version+"<br>";
+	document.getElementById("version").innerHTML = "Version:<br>"+version+"<br>";
+	document.getElementById("controls").innerHTML = 
+		"Controls:<br>"+
+		"W, A, S, D to move<br>"+
+		"Mouse to aim<br>"+
+		"Spacebar to shoot<br>"+
+		"Switching Weapons:<br>"+
+		"1 - Beam<br>"+
+		"2 - Splitter<br>"+
+		"3 - Heavy<br>"+
+		"4 - Gatling<br>"+
+		"5 - Incinerator<br>"+
+		"Enter thesupersecretcode<br>to activate Omega weapons!<br>"+
+		"+ - to zoom in and out";
 	devtools = new setdevtools();
 
 	loadAssets();
 	cam = new CamMan();
 	c = document.getElementById("canvas");
 	ctx = c.getContext("2d");
+
+	lastT = new Date().getTime();
+	delta = 0;
+
+	resizewin();
 
 	world = new b2World(new b2Vec2(0,0),false);
 	world.SetContactListener(MakeContactListener());
@@ -45,15 +66,37 @@ function init(){
 
 	gos = [];
 	counters = [];
+	buttons = [];
+
 	player = new Player();
 	spawner = new Spawner();
-	for (var i = 0; i < 25; i++)
+	for (var i = 0; i < 105; i++)
 		spawner.spawnonscreen("ufo");
 
-	resizewin();
+	if (localStorage.getItem("omegaunlocked")){
+	new Button("Activate Omega",bounds.hr.x+bounds.hr.w/2,bounds.hr.y+140,200,30,toggleomega)
+		.apply({id:"omegatoggle"});//function(){this.id = "omegatoggle";alert(this.id);});//alert(bb.id);
+	toggleomega();toggleomega();}
+
+
+	new Button("Pause",bounds.hr.x+bounds.hr.w/2,bounds.hr.y+35,200,30,function(){
+			paused = !paused;
+			if (paused) this.label = "Unpause";
+			else this.label = "Pause";
+		});
+	new Button("Mute",bounds.hr.x+bounds.hr.w/2,bounds.hr.y+70,200,30,function(){
+			mute = !mute;
+			if (mute) this.label = "Unmute";
+			else this.label = "Mute";
+		});
+	new Button("Reset All",bounds.hr.x+bounds.hr.w/2,bounds.hr.y+105,200,30,function(){localStorage.clear();location.reload();});
+
+	mute = false;
+	paused = false;
+
 
 	window.onresize = function(){resizewin();}
-	//window.onmousedown = function(){player.lclick = true;}
+	window.onmousedown = checkbuttons;
 	//window.onmouseup = function(){player.lclick = false;}
 
 	document.getElementById("info").innerHTML+="<div id=\"zoom\">1x</div>";
@@ -66,11 +109,11 @@ function init(){
 	if (localStorage.getItem("playerweapon"))
 		player.switchwep(Number(localStorage.getItem("playerweapon")));
 
-
 	mouse = {
 		x: 0,
 		y: 0,
 		get : function(){return {x:(this.x - cam.pos.x)/viewscale/scale, y:(this.y - cam.pos.y)/viewscale/scale};},
+		getscreen : function(){return {x:this.x,y:this.y};}
 	};
 	var mousemove = function(evt){
 		var rect = c.getBoundingClientRect();
@@ -81,14 +124,15 @@ function init(){
 	document.getElementById("canvas").addEventListener('drag',mousemove);
 
 	document.addEventListener('keydown', function(event) {
+		if (paused) return;
 		var code = "thesupersecretcode";
 		if (omega) code = "turnoff";
-		omegaunlock+=alphabet[event.keyCode-65];
-		if (code.indexOf(omegaunlock)!=0)omegaunlock = "";
-		if (omegaunlock == code) toggleomega();
+		omegaunlock+=getletter(event.keyCode);
+		if (code.indexOf(omegaunlock)!==0)omegaunlock = "";
+		if (omegaunlock == code && !(localStorage.getItem("omegaunlocked"))) unlockomega();
 		switch(event.keyCode){
 			case 49:case 50:case 51:case 52:case 53:
-			player.switchwep(event.keyCode-48);break;
+			if(!paused)player.switchwep(event.keyCode-48);break;
 			case 87:	player.m.w = true;	break;
 			case 65:	player.m.a = true;	break;
 			case 83:	player.m.s = true;	break;
@@ -107,21 +151,76 @@ function init(){
 			case 32:	player.space=false;	break;
 		}
 	});
-
 	setInterval(gameloop,1000/60);
 }
 function setdevtools(){
 	this.b2debug = false;
 	this.imgbox = false;
 }
+function getletter(keycode){
+	keycode-=65;
+	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	return alphabet[keycode];
+}
+function checkbuttons(){
+	var m = mouse.getscreen();
+
+	for (var i = 0; i < buttons.length; i++)
+		if (buttons[i].isover(m.x,m.y))
+			buttons[i].onclick();
+}
+function Button(label, x, y, w, h, onclick){
+	this.label = label;
+	this.x = x-w/2;
+	this.y = y-h/2;
+	this.w = w;
+	this.h = h;
+	this.id = "";
+	this.onclick = onclick;
+	this.apply = function(vars){
+		if (vars.label)	this.label = vars.label;
+		if (vars.id)	this.id = vars.id;
+		if (vars.render)this.render = vars.render;
+	}
+	this.render = function(){
+		if (this.isover(mouse.x,mouse.y))
+			ctx.fillStyle = "white";
+		else
+			ctx.fillStyle = "grey";
+		ctx.globalAlpha=0.5;
+		ctx.fillRect(this.x,this.y,this.w,this.h);
+		ctx.globalAlpha=1;
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.font = "20px Arial";
+		ctx.fillStyle = "black";
+		ctx.fillText(this.label,this.x+this.w/2,this.y+this.h/2);
+	}
+	this.isover = function(x, y){
+		return x >= this.x && x <= this.x+this.w && y >= this.y && y <= this.y+this.h;}
+	buttons.push(this);
+}
+function getbutton(id){
+	for (var i = 0; i < buttons.length; i++)
+		if (buttons[i].id == id)
+			return buttons[i];
+}
+function unlockomega(){
+	if (!omega) alert("Omega weapons unlocked. Beware of lag when using.");
+	localStorage.setItem("omegaunlocked",true);
+	new Button("Activate Omega",bounds.hr.x+bounds.hr.w/2,bounds.hr.y+140,200,30,toggleomega)
+		.apply({id:"omegatoggle"});
+}
 function toggleomega(){
-	if (!omega){ alert("Omega weapons activated. Enter 'turnoff' to deactivate.");alert("Beware of lag.");}
-	else alert("Omega weapons deactivated.");
 	omega = !omega;
-	if (omega) 
-	localStorage.setItem("omega",1);
-	else 
-	localStorage.setItem("omega",-1);
+	if (omega){
+		localStorage.setItem("omega",1);
+		getbutton("omegatoggle").label = "Deactivate Omega";}
+	else{
+		localStorage.setItem("omega",-1);
+		getbutton("omegatoggle").label = "Activate Omega";}
+	for (var i = 0; i < player.arsenal.length; i++)
+		player.arsenal[i].dispose();
 	player.arsenal = [new pBeam(),new pSplitter(),new pHeavy(),new pGatling(),new pIncinerator()];
 	player.switchwep(Number(localStorage.getItem("playerweapon")));
 }
@@ -138,11 +237,16 @@ function zoom(dir){
 function resizewin(){
 	document.getElementById("canvas").width = window.innerWidth-250;
 	document.getElementById("canvas").height = document.documentElement.clientHeight-20;
+	var tb = {w:c.width,h:c.height};
+	bounds = {g:{x:0,	y:0,		w:tb.w*4/5,	h:tb.h-tb.w*1/5},
+		 hb:{x:0,	y:tb.h-tb.w*1/5,w:tb.w*4/5,	h:tb.w*1/5},
+		 hr:{x:tb.w*4/5,y:0,		w:tb.w*1/5,	h:tb.h-tb.w*1/5},
+		 hbr:{x:tb.w*4/5,y:tb.h-tb.w*1/5,w:tb.w*1/5,h:tb.w*1/5}};
 	dostars();
 }
 function dostars(){
 	stars = [];
-	for (var i = 0; i < c.width*c.height/3000/viewscale; i++)
+	for (var i = 0; i < bounds.g.w*bounds.g.h/3000/viewscale; i++)
 		stars.push(new Star());
 }
 function activatedebug(){
@@ -154,6 +258,7 @@ function activatedebug(){
 	world.SetDebugDraw(debugDraw);
 }
 function gameloop(){
+	if (!paused)
 	update();
 	render();
 }
@@ -180,6 +285,25 @@ function render(){
 			if (gos[i].rl==j)
 				gos[i].render();
 	ctx.restore();
+	if (paused){
+		ctx.fillStyle = "grey";
+		ctx.globalAlpha=0.5;
+		ctx.fillRect(0,0,c.width,c.height);
+		ctx.globalAlpha=1;
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.font = "30px Arial";
+		ctx.fillStyle = "white";
+		ctx.fillText("Paused",bounds.g.w/2,bounds.g.h/2);
+		ctx.font = "31px Arial";
+		ctx.fillStyle = "black";
+		ctx.strokeText("Paused",bounds.g.w/2,bounds.g.h/2);
+	}
+	ctx.clearRect(bounds.hb.x,bounds.hb.y,bounds.hb.w,bounds.hb.h);
+	ctx.clearRect(bounds.hr.x,bounds.hr.y,bounds.hr.w,bounds.hr.h);
+	ctx.clearRect(bounds.hbr.x,bounds.hbr.y,bounds.hbr.w,bounds.hbr.h);
+	for (var i = 0; i < buttons.length; i++)
+		buttons[i].render();
 }
 function CamMan() {
 	this.pos = {
@@ -188,8 +312,8 @@ function CamMan() {
 	};
 	this.step = function() {
 		var v = player.p;
-		this.pos.x = -v.x * scale * viewscale + c.width/2;
-		this.pos.y = -v.y * scale * viewscale + c.height/2;
+		this.pos.x = -v.x * scale * viewscale + bounds.g.w/2;
+		this.pos.y = -v.y * scale * viewscale + bounds.g.h/2;
 		ctx.translate(this.pos.x, this.pos.y);
 	};
 }
@@ -305,6 +429,7 @@ function Player(){
 	this.angle = Math.atan2(this.aim.x,this.aim.y);
 	this.arsenal = [new pBeam(),new pSplitter(),new pHeavy(),new pGatling(),new pIncinerator()];//,new pMissile()];
 	this.curwep = this.arsenal[0];
+	localStorage.setItem("playerweapon",""+1);
 	this.body.SetUserData(this);
 	gos.push(this);
 	this.shoot = function(){
@@ -326,7 +451,7 @@ function Player(){
 	this.collide = function(other){
 		switch(other.categ){
 			case "proj":
-				if (other.source != "p")
+				if (other.source !== "p")
 					this.health-=other.damage;
 				break;
 			case "enem":
@@ -377,7 +502,7 @@ function Enemy(){
 	this.collide = function(other){
 		switch(other.categ){
 			case "proj":
-				if (other.source != "e")
+				if (other.source !== "e")
 					this.health-=other.damage;
 				break;
 			case "play":
@@ -421,45 +546,63 @@ function Spawner(){
 	}
 	this.spawnonscreen = function(clss){
 		var x, y;
-		x = (Math.random()-.5)*76*2;
-		y = (Math.random()-.5)*52*2;
+		x = (Math.random()-.5)*100*2;
+		y = (Math.random()-.5)*80*2;
 		x+=player.p.x;
 		y+=player.p.y;
 		this.ufo.spawn(new b2Vec2(x,y));
 	}
 }
 function UFO(){
+	this.categ = "enem";
+	this.clss = "ufo";
 	this.speed = 7;
 	this.maxhealth = 10;
+	this.health = this.maxhealth;
+	this.rl = 0;
 	this.arsenal = [new pBeam()];
 	this.curwep = this.arsenal[0];
+	this.collide = function(other){
+		switch(other.categ){
+			case "proj":
+				if (other.source !== "e")
+					this.health-=other.damage;
+				break;
+			case "play":
+				//body damage?
+				break;
+		}
+	}
 	this.spawn = function(pos){
-		var e = new Enemy();
-		e.clss = "ufo";
-		e.maxhealth = this.maxhealth;
-		e.health = this.maxhealth;
-		e.speed = this.speed;
-		e.p = pos;
-		e.aim = new b2Vec2(0,1);
-		e.angle = Math.atan2(e.aim.y,e.aim.x);
-		e.arsenal = this.arsenal;
-		e.curwep = this.curwep;
-		e.update = function(){
-			if (this.health <= 0 || Math.abs(this.p.x-player.p.x)>80 || Math.abs(this.p.y-player.p.y)>55)
-				this.die();
-			this.p = this.body.GetPosition();
-			this.aim = new b2Vec2(player.p.x-this.p.x,player.p.y-this.p.y);
-			this.angle = Math.atan2(this.aim.y,this.aim.x);
-		}
-		e.render = function(){
-			dynamicdraw(iufo,this.p.x,this.p.y,0,1,1);
-			dynamicdraw(iufoeye,this.p.x,this.p.y,this.angle,1,1);
-			if (this.health>0)
-				dynamicdraw(ihealthbar,this.p.x,this.p.y+1.4,Math.PI/2,this.health/this.maxhealth,1);
-		}
-		e.body = circlebody(1,e.p.x,e.p.y);
-		e.body.SetUserData(e);
-		gos.push(e);
+		var u = new UFO();
+		u.p = pos;
+		u.aim = new b2Vec2(0,1);
+		u.angle = Math.atan2(u.aim.y,u.aim.x);
+		
+		u.body = circlebody(1,u.p.x,u.p.y);
+		u.body.SetUserData(u);
+		gos.push(u);
+	}
+	this.update = function(){
+		if (this.health <= 0 || Math.abs(this.p.x-player.p.x)>100 || Math.abs(this.p.y-player.p.y)>80)
+			this.die();
+		this.p = this.body.GetPosition();
+		this.aim = new b2Vec2(player.p.x-this.p.x,player.p.y-this.p.y);
+		this.angle = Math.atan2(this.aim.y,this.aim.x);
+	}
+	this.render = function(){
+		dynamicdraw(iufo,this.p.x,this.p.y,0,1,1);
+		dynamicdraw(iufoeye,this.p.x,this.p.y,this.angle,1,1);
+		if (this.health>0)
+			dynamicdraw(ihealthbar,this.p.x,this.p.y+1.4,Math.PI/2,this.health/this.maxhealth,1);
+	}
+	this.die = function(){
+		spawner.spawn(this.clss);
+		this.dispose();
+	}
+	this.dispose = function(){
+		gos.splice(gos.indexOf(this),1);
+		world.DestroyBody(this.body);
 	}
 }
 function Counter(len){
@@ -548,7 +691,7 @@ function pBeam(){
 				gos.push(p[i]);
 				t = 0;t0=0;
 			}
-			fxbeam.play();
+			if (!mute) fxbeam.play();
 		}
 		else{
 			var p = new pBeam();
@@ -558,7 +701,7 @@ function pBeam(){
 			info.a.Multiply(p.speed);
 			p.body.SetLinearVelocity(info.a);
 			p.body.SetUserData(p);
-			fxbeam.play();
+			if (!mute) fxbeam.play();
 			gos.push(p);
 		}
 	}
@@ -578,11 +721,11 @@ function pBeam(){
 	this.collide = function(other){
 		switch(other.categ){
 			case "play":
-				if (this.source != "p")
+				if (this.source !== "p")
 					this.pierce--;
 				break;
 			case "enem":
-				if (this.source != "e")
+				if (this.source !== "e")
 					this.pierce--;
 				break;
 		}
@@ -591,6 +734,7 @@ function pBeam(){
 	this.outofrange = function(){this.dispose();}
 	this.dispose = function(){
 		gos.splice(gos.indexOf(this),1);
+		if (this.body)
 		world.DestroyBody(this.body);
 		this.rate.dispose();
 	}
@@ -602,12 +746,12 @@ function pSplitter(){
 	this.rate.loop = true;
 	this.rate.makeready();
 	this.damage = 2;
-	this.range = 85;
+	this.range = 65;
 	this.dt = 0;
 	this.speed = 35;
 	this.maxbranches = 3;
 	this.branches = this.maxbranches+1;
-	this.children = 12;
+	this.children = 6;
 	this.nburst = 0;
 	this.pierce = 0;
 	this.rl = -1;
@@ -621,14 +765,14 @@ function pSplitter(){
 		this.speed = 50;
 		this.maxbranches = 5;
 		this.branches = this.maxbranches+1;
-		this.children = 30;
+		this.children = 25;
 		this.pierce = 3;
 	}
 	this.spawn = function(info){
 		var p = new pSplitter();
 		p.source = info.s;
 		p.branches = this.branches-1;
-		if (p.branches == this.maxbranches)	fxbeam.play();
+		if (p.branches == this.maxbranches && !mute)	fxbeam.play();
 		info.a.Normalize();
 		p.body = circlebody(p.img.width/scale/4,info.p.x+info.a.x*3,info.p.y+info.a.y*3,true);
 		info.a.Multiply(p.speed);
@@ -655,7 +799,7 @@ function pSplitter(){
 	}
 	this.burst = function(count){
 		if (this.branches > 0){
-			fxspark.play();
+			if (!mute) fxspark.play();
 			for (var i = 0; i < count; i++){
 				var t = ((Math.random()-.5)+i)*2*Math.PI/count;
 				this.spawn({p : this.body.GetPosition(), a : new b2Vec2(Math.cos(t),Math.sin(t)), s : this.source});
@@ -673,11 +817,11 @@ function pSplitter(){
 		var hit = false;
 		switch(other.categ){
 			case "play":
-				if (this.source != "p")
+				if (this.source !== "p")
 					hit = true;
 				break;
 			case "enem":
-				if (this.source != "e")
+				if (this.source !== "e")
 					hit = true;
 				break;
 		}
@@ -692,6 +836,7 @@ function pSplitter(){
 	this.outofrange = function(){this.dispose();}
 	this.dispose = function(){
 		gos.splice(gos.indexOf(this),1);
+		if (this.body)
 		world.DestroyBody(this.body);
 		this.rate.dispose();
 	}
@@ -734,7 +879,7 @@ function pHeavy(){
 			p.body.SetLinearVelocity(info.a);
 			p.body.SetUserData(p);
 		}
-		fxheavy.play();
+		if (!mute) fxheavy.play();
 		gos.push(p);
 	}
 	this.update = function(){
@@ -754,11 +899,11 @@ function pHeavy(){
 	this.collide = function(other){
 		switch(other.categ){
 			case "play":
-				if (this.source != "p")
+				if (this.source !== "p")
 					this.pierce--;
 				break;
 			case "enem":
-				if (this.source != "e")
+				if (this.source !== "e")
 					this.pierce--;
 				break;
 		}
@@ -767,6 +912,7 @@ function pHeavy(){
 	this.outofrange = function(){this.dispose();}
 	this.dispose = function(){
 		gos.splice(gos.indexOf(this),1);
+		if (this.body)
 		world.DestroyBody(this.body);
 		this.rate.dispose();
 	}
@@ -794,7 +940,7 @@ function pGatling(){
 		this.pierce = 5;
 	}
 	this.spawn = function(info){
-		fxgatling.play();
+		if (!mute) fxgatling.play();
 		var p = [new pGatling(),new pGatling()];
 		if (omega) p = [new pGatling(),new pGatling(),new pGatling(),new pGatling(),new pGatling(),new pGatling(),
 			new pGatling(),new pGatling(),new pGatling(),new pGatling()];
@@ -830,11 +976,11 @@ function pGatling(){
 	this.collide = function(other){
 		switch(other.categ){
 			case "play":
-				if (this.source != "p")
+				if (this.source !== "p")
 					this.pierce--;
 				break;
 			case "enem":
-				if (this.source != "e")
+				if (this.source !== "e")
 					this.pierce--;
 				break;
 		}
@@ -843,6 +989,7 @@ function pGatling(){
 	this.outofrange = function(){this.dispose();}
 	this.dispose = function(){
 		gos.splice(gos.indexOf(this),1);
+		if (this.body)
 		world.DestroyBody(this.body);
 		this.rate.dispose();
 	}
@@ -864,14 +1011,14 @@ function pIncinerator(){
 		this.rate = new Counter(40);
 		this.rate.loop = true;
 		this.rate.makeready();
-		this.range = 60;
+		this.range = 45;
 		this.damage = 5;
 		this.dt = 0;
 		this.speed = 65;
 		this.pierce = 7;
 	}
 	this.spawn = function(info){
-		fxfire.play();
+		if (!mute) fxfire.play();
 		if (omega){
 			for (var j = 0; j < 2; j++)
 			for (var i = 0; i < Math.PI*2; i+=Math.PI/90){
@@ -921,11 +1068,11 @@ function pIncinerator(){
 	this.collide = function(other){
 		switch(other.categ){
 			case "play":
-				if (this.source != "p")
+				if (this.source !== "p")
 					this.pierce--;
 				break;
 			case "enem":
-				if (this.source != "e")
+				if (this.source !== "e")
 					this.pierce--;
 				break;
 		}
@@ -934,6 +1081,7 @@ function pIncinerator(){
 	this.outofrange = function(){this.dispose();}
 	this.dispose = function(){
 		gos.splice(gos.indexOf(this),1);
+		if (this.body)
 		world.DestroyBody(this.body);
 		this.rate.dispose();
 	}
@@ -941,7 +1089,7 @@ function pIncinerator(){
 function Star(){
 	this.rl = -2;
 	this.p = randomonscreen();
-	this.z = Math.random()*20-10;
+	//this.z = Math.random()*20-10;
 	this.scale = Math.random()*1.5+.2;
 	this.update = function(){
 		var n = player.body.GetLinearVelocity().Copy();
@@ -954,8 +1102,8 @@ function Star(){
 	}
 	this.keeponscreen = function(){
 		var b = player.body.GetPosition(),
-		w = c.width/scale/viewscale,
-		h = c.height/scale/viewscale;
+		w = bounds.g.w/scale/viewscale,
+		h = bounds.g.h/scale/viewscale;
 		if (this.p.x < b.x-w/2)
 			this.p.x+=w;
 		else if (this.p.x > b.x+w/2)
@@ -967,5 +1115,5 @@ function Star(){
 	}
 }
 function randomonscreen(){
-	return new b2Vec2((Math.random()*c.width-c.width/2)/scale/viewscale, (Math.random()*c.height-c.height/2)/scale/viewscale);
+	return new b2Vec2(((Math.random()-.5)*bounds.g.w)/scale/viewscale, ((Math.random()-.5)*c.height-c.height/2)/scale/viewscale);
 }
